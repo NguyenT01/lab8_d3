39c27485a6da6fd9d143221a85036a13814f0ebc
AlexSandro2147 <alexsandro2147@gmail.com>
2021-12-11 18:47:53 +0700

    Update ImplHello.java

--- ImplHello.java
+++ ImplHello.java
@@ -4,6 +4,8 @@ public class ImplHello implements Hello {
     
     @Override
     public void printMsg(String age) throws RemoteException {
+        private final int second = 2222;
+
         int a = Integer.parseInt(age);
         int b= 999999;
         System.out.println("Your age: " + a);
