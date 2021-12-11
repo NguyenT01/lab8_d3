5c719f73019c54e46156d81953a318883d3f78fa
NguyenT01 <transinguyen2001@gmail.com>
2021-12-11 18:47:03 +0700

    Update ImplHello.java

--- ImplHello.java
+++ ImplHello.java
@@ -1,7 +1,8 @@
 import java.rmi.RemoteException;
 
 public class ImplHello implements Hello {
-    
+    private final int first = 1111;
+
     @Override
     public void printMsg(String age) throws RemoteException {
         int a = Integer.parseInt(age);
